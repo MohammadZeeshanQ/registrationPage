4281e9378cb4229fcf254cb9eecef7d25c998846
MohammadZeeshanQ <zaqil1976@gmail.com>
2021-03-13 02:25:23 +0300

    Files Init

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# registrationPage
+Designing a basic responsive Login and Registration Page.
---
+++ src/Components/Contents/infoBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function infoBox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
