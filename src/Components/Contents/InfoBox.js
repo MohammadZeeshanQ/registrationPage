10590e53b2366dbdd08850d96a7f942e20fc695a
MohammadZeeshanQ <zaqil1976@gmail.com>
2021-03-15 20:00:24 +0300

    Phase 2: Mobile Responsive and Minor touch up

--- src/Components/Contents/InfoBox.js
+++ src/Components/Contents/InfoBox.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {makeStyles, Grid, Button, Typography, Input} from '@material-ui/core';
 
 //Importing Picture from assets folder
@@ -13,34 +13,83 @@ const useStyles = makeStyles({
 
     root:{
         width:'60%',
-        margin: '0 auto'
+        margin: '0 auto',
+        '@media (max-width:600px)':{
+            width:'80%'
+        },
     },
     wrapper:{
         backgroundColor:'#fcfcfc',
         borderRadius:'1.5em',
-        border:'1px red solid',
     },
     divMotive:{
         padding:'3em 0',
+        '@media (max-width:600px)':{
+            padding:'2em 0 0 0',
+        },
     },
     motiveText:{
         fontFamily:'Sans sarif, Montserrat',
         fontWeight:'800',
         textAlign:'center',
+        '@media (max-width:600px)':{
+            fontWeight:'700',
+        }
     },
     divImage:{
         padding:'1em 0',
     },
     imagePerson:{
         width:'100%',
-    }
+    },
+    divForm:{
+        textAlign: 'center',
+        padding:'3em 0 1em 0',
+        '@media (max-width:600px)':{
+            padding:'1em 0 0 0',
+        }
+    },
+    divTextContent:{
+        '@media (max-width:600px)':{
+            padding:'1em 0',
+        }
+    },
+    textContentHeading:{
+        fontFamily:'Sans sarif, Montserrat',
+        fontWeight:'700',
+        '@media (max-width:600px)':{
+            fontSize:'1.9em',
+        }
+    },
+    divInput:{
+        margin:'1.2em 0',
+    },
+    divBtn:{
+        margin:'2em 0',
+    },
+    registerBtn:{
+        backgroundColor:'#6a1b9a',
+        color:'white',
+        '&:hover':{
+            transform: 'scale(1.1)',
+            transition: '0.4s',
+        },
+    }, 
+    socialContent:{
+        textAlign:'center',
+        '@media (max-width:600px)':{
+            padding:'2em 0',
+        }
+    },
 
 });
 
 
 export default function InfoBox() {
 
+
     const classes = useStyles();
+
 
     return (
         <div className={classes.root}>
@@ -61,7 +110,7 @@ export default function InfoBox() {
                             xs='9'
                             md='6'
                         >
-                            <div className={classes.gridItem}>
+                            <div className={classes.gridItemPicture}>
 
                                 <div className={classes.divMotive}>
 
@@ -72,9 +121,7 @@ export default function InfoBox() {
                                 </div>
 
                                 <div className={classes.divImage}>
-
                                     <img src={PersonImage} alt='Person Image' className={classes.imagePerson}/>
-
                                 </div>
 
                             </div>
@@ -86,21 +133,30 @@ export default function InfoBox() {
                             xs='9'
                             md='6'
                         >
-                            <div className={classes.gridItem}>
+                            <div className={classes.gridItemText}>
+
+                                
 
                                 <div className={classes.divForm}>
+
+                                    <div className={classes.divTextContent}>
+                                        <Typography className={classes.textContentHeading} variant='h5'> 
+                                            Sign In
+                                        </Typography>
+                                    </div>
+
                                     <form className={classes.formContent} noValidate autoComplete='off'>
-                                        <div>
+                                        <div className={classes.divInput}>
                                             <Input className={classes.inputContent} id='name-Input' placeholder="Username"/>
                                         </div>
-                                        <div>
+                                        <div className={classes.divInput}>
                                             <Input className={classes.inputContent} id='email-Input' placeholder='Email' />   
                                         </div>
-                                        <div>
+                                        <div className={classes.divInput}>
                                             <Input className={classes.inputContent} id='Password' placeholder='Password' type='password' />
                                         </div> 
 
-                                        <div>
+                                        <div className={classes.divBtn}>
                                             <Button className={classes.registerBtn} variant='contained' >Register</Button>
                                         </div>
 
