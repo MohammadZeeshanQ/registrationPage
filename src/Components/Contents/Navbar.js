52bece517e0019a6ad0e8bfcb7e541cffcbdce09
Zeeshan <zaqil1976@gmail.com>
2021-04-01 20:36:47 +0300

    Update: Added the nagivation Bar

--- src/Components/Contents/Navbar.js
+++ src/Components/Contents/Navbar.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import {makeStyles, AppBar, Toolbar, IconButton} from '@material-ui/core';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        flexGrow: 1,
+    },
+    appBar:{ 
+        height: 60,
+        backgroundColor: '#6a1b9a',
+    },
+    toolBar:{
+
+    },
+    logoBtn:{
+        margin:'1em 0',
+    },
+}));
 
 export default function Navbar() {
+
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
+            <AppBar position='fixed' className={classes.appBar}>
+                <Toolbar className={classes.toolBar}>
+                    <IconButton size='small' className={classes.logoBtn}>
+                        <PinterestIcon style={{fontSize: '2em', color:'#FFFFFF',}} />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
             
         </div>
     )
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 //importing Register box modules
 import RegisterBox from './Contents/Background.js';
-
+import NavigationBar from './Contents/Navbar.js';
 
 export default function MainComponent() {
 
     return (
         <div className="root">
+            <NavigationBar/>
             <RegisterBox/>
         </div>
     )
